81eb27e6eb8d53b14780818de7c41c3eb2a2d67f
Mac User <macuser@Mac-Users-iMac-6.local>
2019-01-24 10:55:53 -0800

    Game now maps cards properly, cards holds a clicked state that toggles on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,30 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import './style.css'
 
-function Card(props){
-    return(
-        <Grid item xs={3}>
-            <Paper>    
-                <img src = {props.img} />
-            </Paper>
-        </Grid>
-    )
-    
+class Card extends React.Component{
+    state={
+        clicked: false,
+    };
+
+    handleClick = e=>{
+        if(!this.state.clicked){
+            // add to the score
+            // scramble the order of stuff
+            this.setState({clicked:true});
+        } else{
+            alert('game over');
+        }
+    }
+
+    render(){
+        return(
+            <Grid item xs={3}>
+                <Paper>    
+                    <img src = {this.props.img} onClick={this.handleClick}/>
+                </Paper>
+            </Grid>
+        )
+    }
 }
 
 export default Card
--- src/components/Game.js
+++ src/components/Game.js
@@ -2,31 +2,38 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from './Card';
 import Paper from '@material-ui/core/Paper';
+import bojack from './bojack.json'
 
-function Game(props){
-    return(
-        <div>
-            <Grid container spacring={24}>
-                <Card img='https://pixel.nymag.com/imgs/daily/vulture/2018/09/14/14-bojack-horseman.w700.h700.jpg'>
-                </Card>
-                <Grid item xs={3}>
-                    <Paper>    
-                        <p>image goes here</p>
-                    </Paper>
+class Game extends React.Component{
+    state ={
+        bojack
+    }
+    render(){
+        return(
+            <div>
+                <Grid container spacring={24}>
+                    {this.props.list.map(item=>(
+                        <Card key={item.id} img = {item.url}/>
+                    ))}
+                    {/* <Card img='https://pixel.nymag.com/imgs/daily/vulture/2018/09/14/14-bojack-horseman.w700.h700.jpg' />
+                    <Grid item xs={3}>
+                        <Paper>    
+                            <p>image goes here</p>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper>    
+                            <p>image goes here</p>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Paper>    
+                            <p>image goes here</p>
+                        </Paper>
+                    </Grid> */}
                 </Grid>
-                <Grid item xs={3}>
-                    <Paper>    
-                        <p>image goes here</p>
-                    </Paper>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper>    
-                        <p>image goes here</p>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </div>
-    )
+            </div>
+    )}
 }
 
 export default Game
